a2fa3ffcc60108538d0d487b59de9ba368df0ffe
Nine0512 <nineza0808@gmail.com>
2025-04-14 03:02:36 +0700

    re min size window

--- electron/main.js
+++ electron/main.js
@@ -23,7 +23,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
-    minWidth: 600,
+    minWidth: 400,
     minHeight: 800,
     show: false, // Don't show until content is ready
     webPreferences: {
